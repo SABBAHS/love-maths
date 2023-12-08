22449816b379dbfad02f545fc4c9a62cf1ff8177
SABBAHS <>
2023-12-08 20:36:14 +0000

    display additional questions to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded",function () {
-
     let buttons = document.getElementsByTagName("button");
     for(let button of buttons) {
     button.addEventListener("click",function() {
@@ -7,20 +6,33 @@ document.addEventListener("DOMContentLoaded",function () {
         alert("You Clicked Submit!");
       } else {
          let gameType = this.getAttribute("data-type");
-         alert(`You clicked ${gameType}`);
+         rungame(gameType);
      }
     })
   }
+    rungame("addition");
 })
+
 
 /**The main game "loop", called when the script is first loaded */
 /**and after the users answer is processed */
-function rungame () {
+function rungame(gameType) {
     //creates two random numbers from 1 - 25 //
 
     let num1 = Math.floor (Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+if (gameType === "addition") {
+displayAdditionQuestion(num1, num2);
+} else {
+    alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}.aborting!`;
+
 }
+
+}
+
+
 
 function checkAnswer () {
 
@@ -38,7 +50,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+
 
 }
 function displaySubtractQuestion() {
