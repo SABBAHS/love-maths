8fbba2884d19d11d39b97702ee628d62302ed7d2
SABBAHS <>
2023-12-08 20:03:26 +0000

    rungame function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
-function rungame () {
+document.addEventListener("DOMContentLoaded",function () {
 
+    let buttons = document.getElementsByTagName("button");
+    for(let button of buttons) {
+    button.addEventListener("click",function() {
+    if (this.getAttribute(data-type) === "submit") {
+        alert("You Clicked Submit!");
+      } else {
+         let gameType = this.getAttribute("data-type");
+         alert(`You clicked ${gameType}`);
+     }
+    })
+  }
+})
+
+/**The main game "loop", called when the script is first loaded */
+/**and after the users answer is processed */
+function rungame () {
+    //creates two random numbers from 1 - 25 //
+
+    let num1 = Math.floor (Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer () {
